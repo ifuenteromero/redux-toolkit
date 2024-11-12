ef12d48e4614cb83d1093fa0ab2bef546c6c19da
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-11-12 19:02:14 +0100

    Install prettier and format

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useAppDispatch, useAppSelector } from './app/hooks';
 import { amountAdded, incremented } from './features/counter/counter-slice';
 
 function App() {
-	const count = useAppSelector((state) => state.counter.value);
+	const count = useAppSelector(state => state.counter.value);
 	const dispatch = useAppDispatch();
 	const handleIncrement = () => {
 		dispatch(incremented());
@@ -15,7 +15,12 @@ function App() {
 
 	return (
 		<>
-			<button onClick={handleIncrement}>count is {count} </button>
+			<button
+				className='lala'
+				onClick={handleIncrement}
+			>
+				count is {count}
+			</button>
 			<button onClick={handleIncrementAmount}>count is {count} </button>
 		</>
 	);
